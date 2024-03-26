5ae78eacd062cf5c2e7ac2e44551dd2b51fbbc24
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2024-03-26 22:33:15 +0530

    Remove overriding style class name

--- frontend/src/Components/Cart/CartItem.jsx
+++ frontend/src/Components/Cart/CartItem.jsx
@@ -1,12 +1,12 @@
 function CartItem(food){
     return (
     <tr className="cart-item">
-        <td className="left">
-                <img src={food.imgURL} alt={food.foodName} />
+        <td>
+              <img src={food.imgURL} alt={food.foodName} />
         </td>
-        <td className="right">
-            <h1 className="foodname">{food.foodName}</h1>
-            <h2 className="servings">{"Serves " + food.servings}</h2>
+        <td>
+            <h1>{food.foodName}</h1>
+            <h2>{"Serves " + food.servings}</h2>
         </td>
         <td>
           <h1>{food.price}</h1>
@@ -14,12 +14,6 @@ function CartItem(food){
         <td>
           <input type="number" placeholder="2"></input>
         </td>
-        {/* <div className="bottom">
-          <button><i class="fa-solid fa-plus"></i></button>
-          <label>Qty</label><input type="text" placeholder="1"></input>
-          <button><i class="fa-solid fa-minus"></i></button>
-          <button><i class="fa-solid fa-trash"></i></button>
-        </div> */}
     </tr>)
 }
 
