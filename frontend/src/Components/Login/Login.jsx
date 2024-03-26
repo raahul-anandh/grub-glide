ba58b29a1883510bf25c4fe1269403bfb54873d7
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2024-03-26 20:00:19 +0530

    Add route to registration page

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "../styles/AuthPage.css";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
   return (
     <div className="auth-page">
       <h2>Login</h2>
@@ -15,6 +17,11 @@ function Login() {
           <input type="password" />
         </div>
         <button type="submit">Login</button>
+        <div className="link">
+          <a onClick={() => {
+            navigate("/register")
+          }}>Do not have an account? Sign Up right now!!</a>
+        </div>
       </form>
     </div>
   );
